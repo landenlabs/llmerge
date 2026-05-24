fa83392eb9924cdb13c7786f72943be80610971e
Dennis Lang <landenlabs2@gmail.com>
2026-05-24 19:24:24 -0400

    sync versions

--- llmerge/llmerge.cpp
+++ llmerge/llmerge.cpp
@@ -27,7 +27,7 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#define VERSION "v6.05.05"
+#define VERSION "v6.05.25"
 
 #include "lldiff.hpp"
 #include "Colors.hpp"
